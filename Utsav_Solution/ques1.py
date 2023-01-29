76c7eb4db943df4685ea1cc69119d271e9e0bdfc
utsav-desai <utsavmdesai@gmail.com>
2023-01-29 14:44:00 +0530

    updated

--- Utsav_Solution/ques1.py
+++ Utsav_Solution/ques1.py
@@ -71,32 +71,49 @@ for j, path in enumerate(listdir(dataset_folder)):
 
 saving_folder = '/Users/utsavmdesai/Documents/SEM 6/CS 763/Task1/Solution/Blurred_images/'
 pickle_folder = '/Users/utsavmdesai/Documents/SEM 6/CS 763/Task1/pickle_files'
+
+
+print(listdir(dataset_folder))
+print(listdir(pickle_folder))
+
+
 def Gaussian_Blur(dataset_folder, saving_folder, pickle_folder):
    i = 0
-   for path in listdir(dataset_folder):
-      if (path.endswith(".jpeg")):
-         img_path = "/Users/utsavmdesai/Documents/SEM 6/CS 763/Task1/Solution/dataset/" + path
-         print(img_path)
-         pic_lis = listdir(pickle_folder)
-         if (pic_lis[i].endswith('.DS_Store')):
-            i += 1
-            continue
-         print(pickle_folder + '/' + listdir(pickle_folder)[i])
-         coord_list = load_coordinates(pickle_folder + '/' + listdir(pickle_folder)[i])
-         i += 1
-         img = cv2.imread(img_path)
+   data_list = listdir(dataset_folder)
+   pic_list = listdir(pickle_folder)
 
-         print(coord_list)
+   if '.DS_Store' in data_list:
+      data_list.remove('.DS_Store')
+   if '.DS_Store' in pic_list:
+      pic_list.remove('.DS_Store')
+
+   print(listdir(dataset_folder))
+   print(listdir(pickle_folder))
+
+   for path in data_list:
+
+      img_path = "/Users/utsavmdesai/Documents/SEM 6/CS 763/Task1/Solution/dataset/" + path
+      print(img_path)
+
+      # if (pic_lis[i].__contains__('.DS_Store')):
+      #    i += 1
+      #    continue
+      print(pickle_folder + '/' + pic_list[i])
+      coord_list = load_coordinates(pickle_folder + '/' + pic_list[i])
+      i += 1
+      img = cv2.imread(img_path)
+
+      print(coord_list)
 
 
-         result_img = img.copy()
-         cv2.imshow('res img', result_img)
-         cv2.waitKey(0)
-         cv2.destroyAllWindows()
+      result_img = img.copy()
+      cv2.imshow('res img', result_img)
+      cv2.waitKey(0)
+      cv2.destroyAllWindows()
 
-         result_img[coord_list[0][1]:coord_list[0][3], coord_list[0][0]:coord_list[0][2],:] = cv2.GaussianBlur(result_img[coord_list[0][1]:coord_list[0][3], coord_list[0][0]:coord_list[0][2], :], (53, 53), 10000)
+      result_img[coord_list[0][1]:coord_list[0][3], coord_list[0][0]:coord_list[0][2],:] = cv2.GaussianBlur(result_img[coord_list[0][1]:coord_list[0][3], coord_list[0][0]:coord_list[0][2], :], (53, 53), 10000)
 
-         cv2.imwrite(saving_folder + str(i) + '.jpg', result_img)
-         print('saving resultant image...')
+      cv2.imwrite(saving_folder + str(i) + '.jpg', result_img)
+      print('saving resultant image...')
 
 Gaussian_Blur(dataset_folder, saving_folder, pickle_folder)
